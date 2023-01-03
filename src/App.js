689168f53fa7d6426b4957e107e59a74832568cd
A.J.K.G <athanasuisgadosey@gmail.com>
2023-01-03 03:05:59 +0000

    installed reactrouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 // jshint esversion:6
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-// import Home from './screens/Home';
-// import Login from './screens/Login';
+import Home from './screens/Home';
+import Login from './screens/Login';
 import Register from './screens/Register';
 
 
 function App() {
   return (
     <div className="App">
-     <Register />
-     {/* <Login />
-     <Register /> */}
+    <Routes>
+      <Route path='/' element={ <Login /> }/>
+      <Route path='/' element={ <Register /> } /> 
+      <Route path='/' element={ <Home /> } />
+    </Routes>
+
     </div>
   );
 }
