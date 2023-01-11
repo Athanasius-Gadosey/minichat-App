629b419d3f60c63ad88fa6a93f797149ebdcb97f
A.J.K.G <athanasuisgadosey@gmail.com>
2023-01-11 00:41:24 +0000

    worked on the search component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,19 +1,90 @@
 // jshint esversion:6
+import { async } from '@firebase/util';
 import { AttachFile, PhotoAlbum } from '@mui/icons-material';
-import React from 'react';
+import { arrayUnion, doc, serverTimestamp, Timestamp, updateDoc } from 'firebase/firestore';
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { ChatContext } from '../context/ChatContext';
+import { db, storage } from '../Firebase';
 import './Input.css';
+import { v4 as uuid} from 'uuid';
+import { updateCurrentUser } from 'firebase/auth';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 
 function Input() {
+  const [text, setText] = useState('');
+  const [img, setImg] = useState(null);
+
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const handleSend = async () =>{
+    if(img) {
+
+      const storageRef = ref(storage, uuid);
+
+      const uploadTask = uploadBytesResumable( storageRef, img );
+
+      uploadTask.on(
+        (error) => {
+          // Handle unsuccessful uploads
+          // setErr(true);
+        }, 
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then( async(downloadURL) => {
+            await updateDoc(doc(db, 'chats', data.chatId),{
+              messages: arrayUnion({
+                id: uuid(), text,
+                senderId: updateCurrentUser.uid,
+                date: Timestamp.now(),
+                img: downloadURL,
+              }),
+            }) 
+        });
+       } 
+      );
+
+
+    }else{
+     await updateDoc(doc(db, 'chats', data.chatId),{
+      messages: arrayUnion({
+        id: uuid(), text,
+        senderId: currentUser.uid,
+        date: Timestamp.now(),
+      }),
+     }); 
+    }
+
+    await updateDoc(doc(db, 'userChats', currentUser.uid), {
+      [data.chatId + '.lastMessage']: {
+        text,
+      },
+      [data.chatId+'.date']: serverTimestamp(),
+    });
+
+    await updateDoc(doc(db, 'userChats', data.user.uid), {
+      [data.chatId + '.lastMessage']: {
+        text,
+      },
+      [data.chatId+'.date']: serverTimestamp(),
+    });
+    
+    setText('');
+    setImg(null);
+  };
+
   return (
     <div className='input'>
-      <input type="text" placeholder='express yourself...' />
+      <input type="text" placeholder='express yourself...' onChange={e=>setText(e.target.value)} value={text} />
       <div className="send">
         <AttachFile className='attach'/>
-        <input type="text" id='file'/>
+        <input type="file" id='file' onChange={e=>setImg(e.target.files[0])}/>
         <label htmlFor="file">
           <PhotoAlbum />
         </label>
-        <button>Send</button>
+        <button onClick={handleSend}>Send</button>
       </div>
     </div>
   )
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,19 +1,31 @@
 // jshint esversion:6
-import React from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import './Message.css';
-import Habib from '../img/habib.jpg';
-import Venti from '../img/venti.jpg'
+// import Habib from '../img/habib.jpg';
+// import Venti from '../img/venti.jpg';
+import { AuthContext } from '../context/AuthContext';
+import { ChatContext } from '../context/ChatContext';
 
-function Message() {
+function Message({message}) {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const ref = useRef()
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({behavior: 'smooth'})
+  }, [message])
+
   return (
-    <div className='message'>
+    <div ref={ref}
+     className={`message ${message.senderId === currentUser.uid}`}>
       <div className="message__info">
-        <img src={Habib} alt="" />
+        <img src={message.senderId === currentUser.uid ? currentUser.photoURL: data.user.photoURL} alt="" />
         <span>just now</span>
       </div>
       <div className="message__content">
-        <p>Whats up?</p>
-        <img src={Venti} alt="" />
+        <p>{message.text}</p>
+        {message.img &&<img src={message.img} alt="" />}
       </div>
     </div>
   )
